08e64d4de6cfcf20f28b3bb8d6bd5a1292069fee
tumero58 <erik.tumasyan@inomma.com>
2023-03-10 11:47:53 +0400

    feat: add connect button

--- application/src/pages/_app.tsx
+++ application/src/pages/_app.tsx
@@ -1,6 +1,40 @@
+import { RdtProvider } from '@/rdt/RdtProvider';
 import '@/styles/globals.css'
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [isMounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (isMounted) {
+    return (
+      <RdtProvider
+        value={RadixDappToolkit(
+          {
+            dAppName: "Radix dApp",
+            dAppDefinitionAddress:
+              "account_tdx_b_1pzv5m8xqy39jmjkk60jluwhrctcs4qpafrxs7rr54jwq0899y3",
+          },
+          (requestData) => {
+            requestData({
+              accounts: { quantifier: "atLeast", quantity: 1 },
+            });
+          },
+          {
+            networkId: 34,
+          }
+        )}
+      >
+        <Component {...pageProps} />
+      </RdtProvider>
+    )
+  } else {
+    return (<></>)
+  }
 }
--- application/src/pages/index.tsx
+++ application/src/pages/index.tsx
@@ -1,5 +1,16 @@
 import Head from 'next/head'
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "radix-connect-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
 export default function Home() {
   return (
     <>
@@ -10,7 +21,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <>
-        <p>Welcome</p>
+      <div>
+        <radix-connect-button></radix-connect-button>
+      </div>
       </>
     </>
   )
