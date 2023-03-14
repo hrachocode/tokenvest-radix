209fd3709457735eb8aa9f45e4427c6f92d98511
tumero58 <erik.tumasyan@inomma.com>
2023-03-14 16:15:27 +0400

    feat: create a single product page

---
+++ application/src/constants/routes.ts
@@ -0,0 +1,13 @@
+export const HOME = "/";
+export const PRODUCTS = "/products";
+
+export const ROUTES = [
+    {
+        title: "Home",
+        slug:HOME
+    },
+    {
+        title: "Products",
+        slug:PRODUCTS
+    },
+];
