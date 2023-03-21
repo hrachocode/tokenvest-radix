0b7e0eaea024631b64c116936852a68f75d10e53
tumero58 <erik.tumasyan@inomma.com>
2023-03-21 14:35:41 +0400

    feat: cleanup useManifest

--- application/src/hooks/useManifest.ts
+++ application/src/hooks/useManifest.ts
@@ -1,37 +1,33 @@
 import { CMS_API, CMS_PRODUCTS, DEFAULT_RAISED_AMOUNT } from "@/constants/cms";
 import { GATEWAY_URL, GATEWAY_URL_RESOURCES, GATEWAY_URL_DETAILS, PACKAGE_ID, TRANSACTION_SUCCESSFUL } from "@/constants/radix";
 import { IProduct } from "@/interfaces/cmsInterface";
+import { ITransactionRes } from "@/interfaces/radixInterface";
 import { handleRequest, METHODS } from "@/utils/handleRequest";
 import { createProductManifest, investManifest } from "@/utils/manifest";
 import { useAccounts } from "./useAccounts";
 import { useSendTransaction } from "./useSendTransaction";
 
-interface ITransactionId {
-    value: {
-        transactionIntentHash: string
-    }
-}
 
 export const useManifest = () => {
     const accounts = useAccounts();
     const sendTransaction = useSendTransaction();
 
     const createProduct = async (title: string, description: string, raiseAmount: string) => {
-        const transactionId = await sendTransaction(createProductManifest(PACKAGE_ID, raiseAmount, title, accounts[0].address));
-        const result = await handleRequest(GATEWAY_URL, METHODS.POST, {
+        const transactionRes = await sendTransaction(createProductManifest(PACKAGE_ID, raiseAmount, title, accounts[0].address));
+        const transactionInfo = await handleRequest(GATEWAY_URL, METHODS.POST, {
             "transaction_identifier": {
                 "type": "intent_hash",
-                "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
+                "value_hex": (transactionRes as ITransactionRes).value.transactionIntentHash
             }
         });
-        if (result.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
+        if (transactionInfo.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
             await handleRequest(`${CMS_API}${CMS_PRODUCTS}`, METHODS.POST, {
                 "data": {
                     "title": title,
                     "description": description,
                     "raiseAmount": raiseAmount,
                     "raisedAmount": DEFAULT_RAISED_AMOUNT,
-                    "componentId": result.details.referenced_global_entities[0],
+                    "componentId": transactionInfo.details.referenced_global_entities[0],
                     "ownerAddress": accounts[0].address
                 }
             })
@@ -39,26 +35,26 @@ export const useManifest = () => {
     };
 
     const invest = async (investAmount: string, product: IProduct) => {
-        const data = await handleRequest(GATEWAY_URL_RESOURCES, METHODS.POST, {
+        const resourcesData = await handleRequest(GATEWAY_URL_RESOURCES, METHODS.POST, {
             "address": accounts[0].address
         })
-        const resources = data.fungible_resources.items;
+        const resources = resourcesData.fungible_resources.items;
         await resources.forEach(async (item: { address: string }) => {
-            const detailsRes = await handleRequest(GATEWAY_URL_DETAILS, METHODS.POST, {
+            const resourceDetails = await handleRequest(GATEWAY_URL_DETAILS, METHODS.POST, {
                 "address": item.address
             });
-            detailsRes.metadata.items.forEach(async (item: { value: string }) => {
+            resourceDetails.metadata.items.forEach(async (item: { value: string }) => {
                 if (item.value === "Radix") {
-                    const radixAddress = detailsRes.address;
-                    const transactionId = await sendTransaction(investManifest(accounts[0].address, investAmount, radixAddress, product.componentId));
-                    const result = await handleRequest(GATEWAY_URL, METHODS.POST, {
+                    const radixAddress = resourceDetails.address;
+                    const transactionRes = await sendTransaction(investManifest(accounts[0].address, investAmount, radixAddress, product.componentId));
+                    const transactionInfo = await handleRequest(GATEWAY_URL, METHODS.POST, {
                         "transaction_identifier": {
                             "type": "intent_hash",
-                            "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
+                            "value_hex": (transactionRes as ITransactionRes).value.transactionIntentHash
                         }
                     });
-                    const amount = result.details.receipt.output[3].data_json;
-                    if (result.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
+                    const amount = transactionInfo.details.receipt.output[3].data_json;
+                    if (transactionInfo.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
                         await handleRequest(`${CMS_API}${CMS_PRODUCTS}/${product.id}`, METHODS.PUT, {
                             "data": {
                                 "raisedAmount": amount,
