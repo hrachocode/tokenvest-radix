0ab4a812e0ca8b02413b3aaf376ef3bdbeadabbb
tumero58 <erik.tumasyan@inomma.com>
2023-03-21 14:25:00 +0400

    feat: cleaup useManifest hook

--- application/src/hooks/useManifest.ts
+++ application/src/hooks/useManifest.ts
@@ -1,6 +1,7 @@
-import { CMS_API, CMS_PRODUCTS } from "@/constants/cms";
+import { CMS_API, CMS_PRODUCTS, DEFAULT_RAISED_AMOUNT } from "@/constants/cms";
 import { GATEWAY_URL, GATEWAY_URL_RESOURCES, GATEWAY_URL_DETAILS, PACKAGE_ID, TRANSACTION_SUCCESSFUL } from "@/constants/radix";
 import { IProduct } from "@/interfaces/cmsInterface";
+import { handleRequest, METHODS } from "@/utils/handleRequest";
 import { createProductManifest, investManifest } from "@/utils/manifest";
 import { useAccounts } from "./useAccounts";
 import { useSendTransaction } from "./useSendTransaction";
@@ -17,102 +18,56 @@ export const useManifest = () => {
 
     const createProduct = async (title: string, description: string, raiseAmount: string) => {
         const transactionId = await sendTransaction(createProductManifest(PACKAGE_ID, raiseAmount, title, accounts[0].address));
-        const data = await fetch(GATEWAY_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "transaction_identifier": {
-                    "type": "intent_hash",
-                    "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
+        const result = await handleRequest(GATEWAY_URL, METHODS.POST, {
+            "transaction_identifier": {
+                "type": "intent_hash",
+                "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
+            }
+        });
+        if (result.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
+            await handleRequest(`${CMS_API}${CMS_PRODUCTS}`, METHODS.POST, {
+                "data": {
+                    "title": title,
+                    "description": description,
+                    "raiseAmount": raiseAmount,
+                    "raisedAmount": DEFAULT_RAISED_AMOUNT,
+                    "componentId": result.details.referenced_global_entities[0],
+                    "ownerAddress": accounts[0].address
                 }
             })
-        });
-        const result = await data.json();
-        if (result.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
-            await fetch(`${CMS_API}${CMS_PRODUCTS}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    "data": {
-                        "title": title,
-                        "description": description,
-                        "raiseAmount": raiseAmount,
-                        "raisedAmount": "0",
-                        "componentId": result.details.referenced_global_entities[0],
-                        "ownerAddress": accounts[0].address
-                    }
-                })
-            })
         }
-        return result;
     };
 
     const invest = async (investAmount: string, product: IProduct) => {
-        const res = await fetch(GATEWAY_URL_RESOURCES, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "address": accounts[0].address
-            })
+        const data = await handleRequest(GATEWAY_URL_RESOURCES, METHODS.POST, {
+            "address": accounts[0].address
         })
-        const data = await res.json();
         const resources = data.fungible_resources.items;
         await resources.forEach(async (item: { address: string }) => {
-            const details = await fetch(GATEWAY_URL_DETAILS, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    "address": item.address
-                })
+            const detailsRes = await handleRequest(GATEWAY_URL_DETAILS, METHODS.POST, {
+                "address": item.address
             });
-            const detailsRes = await details.json();
             detailsRes.metadata.items.forEach(async (item: { value: string }) => {
                 if (item.value === "Radix") {
                     const radixAddress = detailsRes.address;
                     const transactionId = await sendTransaction(investManifest(accounts[0].address, investAmount, radixAddress, product.componentId));
-                    const data = await fetch(GATEWAY_URL, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            "transaction_identifier": {
-                                "type": "intent_hash",
-                                "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
-                            }
-                        })
+                    const result = await handleRequest(GATEWAY_URL, METHODS.POST, {
+                        "transaction_identifier": {
+                            "type": "intent_hash",
+                            "value_hex": (transactionId as ITransactionId).value.transactionIntentHash
+                        }
                     });
-                    const result = await data.json();
                     const amount = result.details.receipt.output[3].data_json;
                     if (result.transaction.transaction_status === TRANSACTION_SUCCESSFUL) {
-                        await fetch(`${CMS_API}${CMS_PRODUCTS}/${product.id}`, {
-                            method: "PUT",
-                            headers: {
-                                "Content-Type": "application/json"
-                            },
-                            body: JSON.stringify({
-                                "data": {
-                                    "raisedAmount": amount,
-                                }
-                            })
+                        await handleRequest(`${CMS_API}${CMS_PRODUCTS}/${product.id}`, METHODS.PUT, {
+                            "data": {
+                                "raisedAmount": amount,
+                            }
                         })
                     }
                 }
             })
         })
-
-
-
-
-
     };
 
 
